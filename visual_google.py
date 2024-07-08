ddba75691e9525e779caba56b1771ef977a5fffa
林志嘉 <57003648+garylin111@users.noreply.github.com>
2024-07-08 16:55:59 +0800

    Update visual_google.py

--- visual_google.py
+++ visual_google.py
@@ -45,6 +45,8 @@ def main():
 def load_data(start_time, end_time, url):
     SCOPES = ('https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive')
     service_account_info = json.dumps(secret_dict)
+    service_account_info = json.loads(secret_json)
+    
     credentials = service_account.Credentials.from_service_account_info(json.loads(service_account_info), scopes=SCOPES)
 
     gc = pygsheets.authorize(custom_credentials=credentials)
