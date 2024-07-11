fe8138ad6d768a2c97cf977243219921825836de
林志嘉 <57003648+garylin111@users.noreply.github.com>
2024-07-11 17:14:12 +0800

    Update visual_google.py

--- visual_google.py
+++ visual_google.py
@@ -10,13 +10,13 @@ import plotly.express as px
 # 設置服務賬號信息
 secret_dict = dict(st.secrets["secret_key"])
 # 標準數内容
-secret_num_dict = dict(st.secrets["data"])
+# secret_num_dict = dict(st.secrets["data"])
 
 
 # Streamlit 页面布局和输入
 def main():
-    data = pd.Dataframe(secret_num_dict)
-    st.write(data)
+    # data = pd.Dataframe(secret_num_dict)
+    # st.write(data)
     st.title('生產計劃')
 
     start_time = st.date_input('訂單開始時間', key='start')
